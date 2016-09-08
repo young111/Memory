7ce183f24a6b13055ee4ed69a0162c5d17a5ce85
YoungSuk Jo <cyeternal524@naver.com>
2016-09-08 20:58:40 +0900

    #3 Event Programming

--- Application/src/main/java/com/example/android/navigationdrawer/NavigationDrawerActivity.java
+++ Application/src/main/java/com/example/android/navigationdrawer/NavigationDrawerActivity.java
@@ -65,7 +65,7 @@ import java.util.Locale;
  * An action should be an operation performed on the current contents of the window,
  * for example enabling or disabling a data overlay on top of the current content.</p>
  */
-public class NavigationDrawerActivity extends Activity implements PlanetAdapter.OnItemClickListener {
+public class NavigationDrawerActivity extends Activity implements PlanetAdapter.OnItemClickListener, View.OnClickListener {
     private DrawerLayout mDrawerLayout;
     private RecyclerView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
@@ -73,6 +73,7 @@ public class NavigationDrawerActivity extends Activity implements PlanetAdapter.
     private CharSequence mDrawerTitle;
     private CharSequence mTitle;
     private String[] mPlanetTitles;
+    static ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -166,12 +167,13 @@ public class NavigationDrawerActivity extends Activity implements PlanetAdapter.
     /* The click listener for RecyclerView in the navigation drawer */
     @Override
     public void onClick(View view, int position) {
+
         selectItem(position);
     }
 
     private void selectItem(int position) {
         // update the main content by replacing fragments
-        Fragment fragment = PlanetFragment.newInstance(position);
+        Fragment fragment = PlanetFragment.newInstance(position, this);
 
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction ft = fragmentManager.beginTransaction();
@@ -209,16 +211,28 @@ public class NavigationDrawerActivity extends Activity implements PlanetAdapter.
     }
 
     /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this,ViewPagerActivity.class);
+        startActivity(intent);
+    }
+
+    /**
      * Fragment that appears in the "content_frame", shows a planet
      */
     public static class PlanetFragment extends Fragment {
         public static final String ARG_PLANET_NUMBER = "planet_number";
-
+        static NavigationDrawerActivity m_nda;
         public PlanetFragment() {
             // Empty constructor required for fragment subclasses
         }
 
-        public static Fragment newInstance(int position) {
+        public static Fragment newInstance(int position, NavigationDrawerActivity nda) {
+            m_nda = nda;
             Fragment fragment = new PlanetFragment();
             Bundle args = new Bundle();
             args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
@@ -235,7 +249,8 @@ public class NavigationDrawerActivity extends Activity implements PlanetAdapter.
 
             int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
                     "drawable", getActivity().getPackageName());
-            ImageView iv = ((ImageView) rootView.findViewById(R.id.image));
+            iv = ((ImageView) rootView.findViewById(R.id.image));
+            iv.setOnClickListener(m_nda);
             iv.setImageResource(imageId);
 
             getActivity().setTitle(planet);
